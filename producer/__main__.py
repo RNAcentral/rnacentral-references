c0e6d34d1ff2a3ac9a17c25a9742e2c82ea59e37
carlosribas <caduribas@gmail.com>
2021-09-10 19:53:06 +0100

    Check pending jobs and send to consumers

--- producer/__main__.py
+++ producer/__main__.py
@@ -20,8 +20,11 @@ from aiojobs.aiohttp import setup as setup_aiojobs
 from aiohttp import web
 
 from . import settings
+from database.job import find_job_to_run
+from database.consumers import find_available_consumers
 from database.models import close_pg, init_pg, migrate
 from database.settings import get_postgres_credentials
+from producer.consumer_jobs import delegate_job_to_consumer
 from .urls import setup_routes
 
 
@@ -34,18 +37,29 @@ async def on_startup(app):
         await migrate(app['settings'].ENVIRONMENT)
 
     # initialize scheduling tasks to consumers in background
-    # await create_consumer_scheduler(app)
+    await create_consumer_scheduler(app)
 
 
 async def check_jobs_and_consumers(app):
     # if there are any pending jobs and free consumers, schedule their execution
-    pass
+    unfinished_job = await find_job_to_run(app['engine'])
+    available_consumers = await find_available_consumers(app['engine'])
+
+    for consumer in available_consumers:
+        if unfinished_job:
+            job = unfinished_job.pop(0)
+            await delegate_job_to_consumer(
+                engine=app['engine'],
+                consumer_ip=consumer.ip,
+                consumer_port=consumer.port,
+                job_id=job[0]
+            )
 
 
 async def create_consumer_scheduler(app):
     """
     Periodically runs a task that checks the status of consumers in the database and
-     - schedules job_chunks to run on consumers
+     - schedules jobs to run on consumers
      - restarts stuck consumers
 
     Stolen from:
@@ -54,7 +68,7 @@ async def create_consumer_scheduler(app):
     async def periodic():
         while True:
             loop.create_task(check_jobs_and_consumers(app))
-            await asyncio.sleep(30)
+            await asyncio.sleep(5)
 
     loop = asyncio.get_event_loop()
     loop.create_task(periodic())
