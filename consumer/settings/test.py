5babc16d04ee183047c676c3f73664252c917e5d
carlosribas <caduribas@gmail.com>
2021-09-13 22:00:41 +0100

    Remove unnecessary variables

--- consumer/settings/test.py
+++ consumer/settings/test.py
@@ -1,14 +1,4 @@
-from . import PROJECT_ROOT
-
-
-# in test save queries and results in a temporary folder
-TMP_DIR = PROJECT_ROOT / '.tmp'
-
-# full path to query files
-RESULTS_DIR = PROJECT_ROOT / '.tmp' / 'results'
-
 # producer server location
 PRODUCER_PROTOCOL = 'http'
 PRODUCER_HOST = 'localhost'
 PRODUCER_PORT = '8080'
-PRODUCER_JOB_DONE_URL = 'api/job-done'
