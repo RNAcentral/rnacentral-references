3077efc4c55347f2fd4e2a542d50f71f4f614bb6
carlosribas <caduribas@gmail.com>
2021-09-26 12:01:58 +0100

    Set directory containing XML files

--- consumer/settings/production.py
+++ consumer/settings/production.py
@@ -1,4 +1,9 @@
+import pathlib
+
 # producer server location
 PRODUCER_PROTOCOL = 'http'
 PRODUCER_HOST = '192.168.0.5'
 PRODUCER_PORT = '8080'
+
+# XML directory
+path_to_xml_files = pathlib.Path('/nfs/references')
--- consumer/settings/test.py
+++ consumer/settings/test.py
@@ -1,4 +1,9 @@
+from consumer.settings import PROJECT_ROOT
+
 # producer server location
 PRODUCER_PROTOCOL = 'http'
 PRODUCER_HOST = 'localhost'
 PRODUCER_PORT = '8080'
+
+# XML directory
+path_to_xml_files = PROJECT_ROOT.parent / 'consumer' / 'files'
