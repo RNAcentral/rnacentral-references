e476cf6a1d00684e04fefdff734322cbd86d1160
carlosribas <caduribas@gmail.com>
2021-09-26 21:02:16 +0100

    Import path_to_xml_files

--- consumer/performance_test/test_in.py
+++ consumer/performance_test/test_in.py
@@ -1,11 +1,10 @@
 import timeit
 
-from consumer.settings import PROJECT_ROOT
+from consumer.settings import path_to_xml_files
 
 
 def test():
     # list of xml files
-    path_to_xml_files = PROJECT_ROOT.parent / 'consumer' / 'files'
     xml_files = [file for file in path_to_xml_files.glob('*.xml') if file.name.startswith('PMC2580001')]
     job_id = "mir-122"
     # there are 238 matches in the whole file, including words like miR-122, hsa-miR-122, and others
--- consumer/performance_test/test_mmap.py
+++ consumer/performance_test/test_mmap.py
@@ -1,13 +1,11 @@
 import mmap
-import re
 import timeit
 
-from consumer.settings import PROJECT_ROOT
+from consumer.settings import path_to_xml_files
 
 
 def test():
     # list of xml files
-    path_to_xml_files = PROJECT_ROOT.parent / 'consumer' / 'files'
     xml_files = [file for file in path_to_xml_files.glob('*.xml') if file.name.startswith('PMC2580001')]
     job_id = b"pre-miR-138"
     # there are 238 matches in the whole file, including words like miR-122, hsa-miR-122, and others
