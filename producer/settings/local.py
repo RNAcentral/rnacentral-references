810f07d12ad863061a39aed8606539356ac10119
carlosribas <caduribas@gmail.com>
2021-10-05 19:26:12 +0100

    Set directory to store XML files for search index

--- producer/settings/local.py
+++ producer/settings/local.py
@@ -1,5 +1,10 @@
+from producer.settings import PROJECT_ROOT
+
 # hostname to listen on
 HOST = '0.0.0.0'
 
 # TCP port for the server to listen on
 PORT = 8080
+
+# path to XML files for search index
+path_to_xml_files = PROJECT_ROOT.parent / 'producer' / 'files'
--- producer/settings/production.py
+++ producer/settings/production.py
@@ -1,5 +1,10 @@
+import pathlib
+
 # hostname to listen on
 HOST = "0.0.0.0"
 
 # TCP port for the server to listen on
 PORT = 8080
+
+# path to XML files for search index
+path_to_xml_files = pathlib.Path('/nfs/references/search_index')
