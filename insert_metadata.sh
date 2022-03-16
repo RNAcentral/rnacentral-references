dfe4d1002361f3eec6a7636c438e73eae85f9f05
carlosribas <caduribas@gmail.com>
2022-03-16 09:05:22 +0000

    Pass database as parameter

--- insert_metadata.sh
+++ insert_metadata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script to insert metadata in RNAcentral-references.
 
-# set the file based on the first argument
+# set file and database
 file=$1
+db=$2
 
 # -o allexport enables all following variable definitions to be exported.
 # +o allexport disables this feature.
@@ -12,17 +13,20 @@ set +o allexport
 
 function insertMetadata
 {
-  # get gene, primary_id and urs
+  # set line and database
   line=$1
+  db=$2
+
+  # get gene, primary_id and urs
   IFS="|" read gene primary_id urs <<< "$line"
 
   # insert data
-  psql -U $username -d $dbname -c "INSERT INTO database (job_id, name, primary_id) VALUES ('$gene', 'rnacentral', '$urs')"
-  psql -U $username -d $dbname -c "INSERT INTO database (job_id, name, primary_id) VALUES ('$primary_id', 'rnacentral', '$urs')"
+  psql -U $username -d $dbname -c "INSERT INTO database (job_id, name, primary_id) VALUES ('$gene', '$db', '$urs')"
+  psql -U $username -d $dbname -c "INSERT INTO database (job_id, name, primary_id) VALUES ('$primary_id', '$db', '$urs')"
 }
 
 # loop through the file
 while IFS="" read -r p || [ -n "$p" ]
 do
-  insertMetadata "$p"
+  insertMetadata "$p" "$db"
 done < "$file"
