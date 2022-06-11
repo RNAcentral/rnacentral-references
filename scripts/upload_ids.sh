aa545fe8086c23b93f28f62b9e1f9f908a06ab5f
carlosribas <caduribas@gmail.com>
2022-06-11 12:23:45 +0100

    Update script to upload ids

---
+++ scripts/upload_ids.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script to submit ids to RNAcentral-references
+# Usage: ./upload_ids.sh [file]
+
+# set parameter
+file=$1
+
+# create folder
+[ ! -d submitted ] && mkdir submitted
+
+function submitJob
+{
+  # set parameter
+  job_id=$1
+
+  # submit job
+  curl -X POST \
+       -H "Content-Type:application/json" \
+       -d "{\"id\": \"${job_id}\"}" \
+       http://45.88.80.122:8080/api/submit-job && echo ${job_id} >> submitted/${file};
+}
+
+# loop through the file
+while IFS="" read -r line || [ -n "$line" ]
+do
+  submitJob "$line"
+done < "$file"
