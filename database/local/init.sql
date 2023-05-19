224ceaea20514daa5124908444444932ee62bc82
carlosribas <caduribas@gmail.com>
2023-05-19 11:32:03 +0100

    Add line to set the owner of the database

--- database/local/init.sql
+++ database/local/init.sql
@@ -1,7 +1,9 @@
 CREATE USER docker WITH ENCRYPTED PASSWORD 'example';
 CREATE DATABASE reference;
 GRANT ALL PRIVILEGES ON DATABASE reference TO docker;
+ALTER DATABASE reference OWNER TO docker;
 ALTER ROLE docker CREATEDB;
 CREATE DATABASE test_reference;
 GRANT ALL PRIVILEGES ON DATABASE test_reference TO docker;
+ALTER DATABASE test_reference OWNER TO docker;
 GRANT ALL ON schema public TO docker;
