05fd9b414b714149505fe1356ef388457ce07867
carlosribas <caduribas@gmail.com>
2023-08-02 16:28:22 +0100

    Unit test fixes

--- producer/tests/test_multiple_jobs.py
+++ producer/tests/test_multiple_jobs.py
@@ -37,50 +37,66 @@ class MultipleJobsTestCase(AioHTTPTestCase):
         return app
 
     @unittest_run_loop
-    async def test_submit_multiple_jobs_post_success(self):
-        data = json.dumps({"job_id": ["FOO.BAR"], "database": "test", "primary_id": "BAR.FOO"})
+    async def test_submit_multiple_jobs_with_id_and_job_list_success(self):
+        data = json.dumps({"job_list": ["FOO.BAR"], "id": "BAR.FOO"})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 201
             text = await response.text()
-            assert text == '{"job_id": ["foo.bar"], "name": "test", "primary_id": "bar.foo"}'
+            assert text == '{"id": "bar.foo", "job_list": ["foo.bar"]}'
 
     @unittest_run_loop
-    async def test_submit_multiple_jobs_missing_job_id(self):
-        data = json.dumps({"database": "test", "primary_id": "BAR.FOO"})
+    async def test_submit_multiple_jobs_only_id_success(self):
+        data = json.dumps({"id": "BAR.FOO"})
+        async with self.client.post(path='/api/multiple-jobs', data=data) as response:
+            assert response.status == 201
+            text = await response.text()
+            assert text == '{"id": "bar.foo"}'
+
+    @unittest_run_loop
+    async def test_submit_multiple_jobs_only_job_list_success(self):
+        data = json.dumps({"job_list": ["FOO.BAR"]})
+        async with self.client.post(path='/api/multiple-jobs', data=data) as response:
+            assert response.status == 201
+            text = await response.text()
+            assert text == '{"job_list": ["foo.bar"]}'
+
+    @unittest_run_loop
+    async def test_submit_multiple_jobs_missing_id_and_job_list(self):
+        data = json.dumps({"test": "test"})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 400
             text = await response.text()
-            assert text == '{"Error": "You must submit a list of job_ids as a parameter"}'
+            assert text == '{"Error": "You must submit id and/or job_list as a parameter"}'
 
     @unittest_run_loop
-    async def test_submit_multiple_jobs_wrong_job_id_type(self):
-        data = json.dumps({"job_id": "foo.bar", "database": "test", "primary_id": "BAR.FOO"})
+    async def test_submit_multiple_jobs_wrong_job_list_type(self):
+        data = json.dumps({"id": "foo.bar", "job_list": "BAR.FOO"})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 400
             text = await response.text()
-            assert text == '{"Error": "You must submit a list of job_ids as a parameter"}'
+            assert text == '{"Error": "You must submit a list of ids as a parameter"}'
 
     @unittest_run_loop
-    async def test_submit_multiple_jobs_missing_database(self):
-        data = json.dumps({"job_id": ["FOO.BAR"], "primary_id": "BAR.FOO"})
+    async def test_submit_multiple_jobs_wrong_id_type(self):
+        data = json.dumps({"id": ["FOO.BAR"]})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 400
             text = await response.text()
-            assert text == '{"Error": "You must submit the database name as a parameter"}'
+            assert text == '{"Error": "You must submit a single id in string format as a parameter"}'
 
     @unittest_run_loop
     async def test_submit_multiple_jobs_rescan_success(self):
-        data = json.dumps({"job_id": ["FOO.BAR"], "database": "test", "primary_id": "BAR.FOO"})
+        data = json.dumps({"job_list": ["FOO.BAR"], "id": "BAR.FOO"})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 201
 
-        data = json.dumps({"job_id": ["FOO.BAR"], "database": "test", "primary_id": "BAR.FOO", "rescan": True})
+        data = json.dumps({"job_list": ["FOO.BAR"], "id": "BAR.FOO", "rescan": True})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 201
 
     @unittest_run_loop
     async def test_submit_multiple_jobs_rescan_fail(self):
-        data = json.dumps({"job_id": ["FOO.BAR"], "database": "test", "primary_id": "BAR.FOO", "rescan": "yes"})
+        data = json.dumps({"job_list": ["FOO.BAR"], "id": "BAR.FOO", "rescan": "yes"})
         async with self.client.post(path='/api/multiple-jobs', data=data) as response:
             assert response.status == 400
             text = await response.text()
@@ -96,7 +112,7 @@ class MultipleJobsTestCase(AioHTTPTestCase):
 
     @unittest_run_loop
     async def test_submit_multiple_jobs_save_metadata(self):
-        data = json.dumps({"job_id": ["FOO.BAR"], "database": "test", "primary_id": "BAR.FOO"})
+        data = json.dumps({"job_list": ["FOO.BAR"], "database": "test", "id": "BAR.FOO"})
         async with self.client.post(path='/api/multiple-jobs', data=data):
             find_metadata_1 = await search_metadata(self.app['engine'], "foo.bar", "test", "bar.foo")
             assert find_metadata_1 is not None
