91c6b415db166831965ca794ad8943b47cd55f68
carlosribas <caduribas@gmail.com>
2024-12-13 11:36:08 +0000

    Add RandomForestClassifier

--- training/text_classification.py
+++ training/text_classification.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
@@ -10,13 +11,13 @@ from sklearn.svm import LinearSVC
 
 def main():
     """
-    Testing three text classifiers: LinearSVC, ComplementNB, and MultinomialNB.
+    Testing four text classifiers: LinearSVC, ComplementNB, MultinomialNB, and RandomForestClassifier.
     """
     df = pd.read_csv("data.csv")
     print(df["rna_related"].value_counts())
     # rna_related
-    # 1    2129
-    # 0    2129
+    # 1    2159
+    # 0    2159
 
     X = df["abstract"]
     y = df["rna_related"]
@@ -34,22 +35,28 @@ def main():
     pipeSVC.fit(X_train, y_train)
     predictSVC = pipeSVC.predict(X_test)
 
+    pipeRF = Pipeline(steps=[("tfidf", TfidfVectorizer()), ("clf", RandomForestClassifier())])
+    pipeRF.fit(X_train, y_train)
+    predictRF = pipeRF.predict(X_test)
+
     print(f"MNB: {accuracy_score(y_test, predictMNB):.2f}")
     print(f"CNB: {accuracy_score(y_test, predictCNB):.2f}")
     print(f"SVC: {accuracy_score(y_test, predictSVC):.2f}")
+    print(f"RF: {accuracy_score(y_test, predictRF):.2f}")
     # MNB: 0.96
     # CNB: 0.96
     # SVC: 0.99
+    # RF: 0.98
 
     print(classification_report(y_test, predictSVC))
     #             precision    recall  f1-score   support
     #
-    #            0       0.99      1.00      0.99       401
-    #            1       1.00      0.99      0.99       451
+    #            0       0.99      1.00      0.99       417
+    #            1       1.00      0.99      0.99       447
     #
-    #     accuracy                           0.99       852
-    #    macro avg       0.99      0.99      0.99       852
-    # weighted avg       0.99      0.99      0.99       852
+    #     accuracy                           0.99       864
+    #    macro avg       0.99      0.99      0.99       864
+    # weighted avg       0.99      0.99      0.99       864
 
 
 if __name__ == '__main__':
