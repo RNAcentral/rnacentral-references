f45916f4c23d36d338003e4415860be6f33a30e0
carlosribas <caduribas@gmail.com>
2024-12-14 16:29:06 +0000

    Get more articles

--- training/export_data_for_manual_checking.py
+++ training/export_data_for_manual_checking.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 EUROPE_PMC: str = "https://www.ebi.ac.uk/europepmc/webservices/rest/search"
 RATE_LIMIT: int = 8
-ARTICLE_LIMIT: int = 20
+ARTICLE_LIMIT: int = 100
 EXCLUDED_KEYWORDS: List[str] = ["non-coding", "ncrna", "lncrna", "sncrna", "mirna"]
 
 
@@ -46,7 +46,7 @@ async def process_abstracts(pmids: Set[str], semaphore: asyncio.Semaphore, rna_r
 
         if rna_related == 0 and any(term in cleaned_abstract for term in EXCLUDED_KEYWORDS):
             continue
-        elif rna_related == 1 and "non-coding" not in cleaned_abstract:
+        elif rna_related == 1 and not any(term in cleaned_abstract for term in EXCLUDED_KEYWORDS):
             continue
         elif "mrna" in cleaned_abstract and number_of_abstracts < ARTICLE_LIMIT:
             processed.append({"abstract": cleaned_abstract, "rna_related": rna_related})
@@ -64,6 +64,9 @@ async def main() -> None:
     """
     list_of_abstracts: List[Dict[str, Any]] = []
 
+    # track processed PMIDs
+    processed_pmids: Set[str] = set()
+
     # use semaphore to fetch abstracts in parallel (respecting the API rate limit)
     semaphore = asyncio.Semaphore(RATE_LIMIT)
 
@@ -73,7 +76,7 @@ async def main() -> None:
                 "query": 'TITLE_ABS:"mrna" AND "ncrna" AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
                 "format": "json",
                 "sort_cited": "y",
-                "pageSize": 100,
+                "pageSize": 1000,
             },
             "rna_related": 1,
         },
@@ -82,40 +85,38 @@ async def main() -> None:
                 "query": 'TITLE_ABS:"mrna" AND NOT ("ncrna" OR "rrna" OR "sncrna") AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
                 "format": "json",
                 "sort_cited": "y",
-                "pageSize": 100,
+                "pageSize": 1000,
             },
             "rna_related": 0,
         },
-        # {
-        #     "params": {
-        #         "query": 'TITLE_ABS:"rna-seq" AND "ncrna" AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
-        #         "format": "json",
-        #         "sort_cited": "y",
-        #         "pageSize": 100,
-        #     },
-        #     "rna_related": 1,
-        # },
-        # {
-        #     "params": {
-        #         "query": 'TITLE_ABS:"rna-seq" AND NOT ("ncrna" OR "rrna" OR "sncrna") AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
-        #         "format": "json",
-        #         "sort_cited": "y",
-        #         "pageSize": 100,
-        #     },
-        #     "rna_related": 0,
-        # },
+        {
+            "params": {
+                "query": 'TITLE_ABS:("rna-seq" AND "ncrna") AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
+                "format": "json",
+                "sort_cited": "y",
+                "pageSize": 1000,
+            },
+            "rna_related": 1,
+        },
+        {
+            "params": {
+                "query": 'TITLE_ABS:("rna-seq" AND NOT ("ncrna" OR "rrna" OR "sncrna")) AND IN_EPMC:Y AND OPEN_ACCESS:Y AND NOT SRC:PPR',
+                "format": "json",
+                "sort_cited": "y",
+                "pageSize": 1000,
+            },
+            "rna_related": 0,
+        },
     ]
-
-    # pmids that mention "rna-seq" but may be unrelated to RNA
-    # rna_seq_0 = {"25516281", "25605792", "25260700", "20979621", "28263959", "20132535", "28991892", "39606674",
-    #              "26925227", "21796119"}
-
-    # pmids that mention "rna-seq" and may be RNA-related
-    # rna_seq_1 = {"32499815", "36395362", "28348640", "34681826", "29073095", "28195222", "32075098", "38832111",
-    #              "38250806", "39494543"}
 
     for query in queries:
         pmids = await fetch_pmids(query["params"])
+
+        # check only new PMIDs
+        new_pmids = pmids - processed_pmids
+        processed_pmids.update(new_pmids)
+
+        # fetch abstracts for the new PMIDs
         abstracts = await process_abstracts(pmids, semaphore, query["rna_related"])
         list_of_abstracts.extend(abstracts)
 
