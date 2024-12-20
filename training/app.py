b4c8e1c6404de10cf0d38b0565a430b207fdfd39
carlosribas <caduribas@gmail.com>
2024-12-20 16:11:35 +0000

    Add code to test with Streamlit

---
+++ training/app.py
@@ -0,0 +1,15 @@
+import joblib
+import streamlit as st
+
+
+model = joblib.load("svc_pipeline.pkl")
+st.title("Text Classification - RNA Related or Not")
+user_input = st.text_area("Enter text for classification:", "")
+
+if st.button("Classify"):
+    if user_input.strip():
+        prediction = model.predict([user_input])[0]
+        result = "RNA-related" if prediction else "Not RNA-related"
+        st.success(f"Prediction: {result}")
+    else:
+        st.warning("Please enter some text before classifying.")
